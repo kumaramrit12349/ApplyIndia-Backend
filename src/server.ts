5ada6604b00aec80713c83ad2e25eaf42c22cdc6
Doli Kumari <kumaridoli1224@gmail.com>
2025-09-07 11:38:50 +0530

    testing

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from "./app";
 import { ENV } from "./config/env";
 
 app.listen(ENV.PORT, () => {
-  console.log(`Server running on http://localhost:${ENV.PORT}`);
+  console.log(`Server running on http://localhost:${ENV.PORT}!`);
 });
