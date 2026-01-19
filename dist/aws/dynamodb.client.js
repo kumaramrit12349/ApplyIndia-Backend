872d7c43fd01668c57d52dd711dac236a43e250d
ApplyIndia <support@applyindia.online>
2026-01-19 21:54:02 +0530

    done some changes.

--- dist/aws/dynamodb.client.js
+++ dist/aws/dynamodb.client.js
@@ -3,12 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dynamoDBClient = void 0;
 const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
 const env_1 = require("../config/env");
-exports.dynamoDBClient = new client_dynamodb_1.DynamoDBClient({
-    region: env_1.AWS_CONFIG.region,
-    credentials: {
-        accessKeyId: env_1.AWS_CONFIG.accessKeyId,
-        secretAccessKey: env_1.AWS_CONFIG.secretAccessKey,
-    },
-    // Optional: safer defaults
-    maxAttempts: 3,
-});
+exports.dynamoDBClient = new client_dynamodb_1.DynamoDBClient(env_1.ENV.APP_ENV === "local"
+    ? {
+        region: env_1.AWS_CONFIG.region,
+        credentials: {
+            accessKeyId: env_1.AWS_CONFIG.accessKeyId,
+            secretAccessKey: env_1.AWS_CONFIG.secretAccessKey,
+        },
+    }
+    : {
+        region: env_1.AWS_CONFIG.region,
+        // 👈 NO credentials in Lambda
+    });
