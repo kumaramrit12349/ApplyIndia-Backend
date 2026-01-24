ae908f8f4b7e13f21b0aea6461e763983418ef2b
ApplyIndia <support@applyindia.online>
2026-01-24 22:52:26 +0530

    s

--- src/config/validateEnv.ts
+++ src/config/validateEnv.ts
@@ -2,9 +2,7 @@
 import { DYNAMODB_CONFIG } from "../config/env";
 
 export function validateEnv() {
-    console.log('DYNAMODB_CONFIG.TABLE_NAME', DYNAMODB_CONFIG.TABLE_NAME);
-  if (!DYNAMODB_CONFIG.TABLE_NAME) {
-
-    throw new Error("DYNAMODB_TABLE_NAME is not defined");
-  }
+if (!process.env.DYNAMODB_TABLE_NAME) {
+  console.error("DYNAMODB_TABLE_NAME missing");
+}
 }
